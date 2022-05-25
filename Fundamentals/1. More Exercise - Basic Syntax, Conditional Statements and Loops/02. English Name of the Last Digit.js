677b51f090f161ef3dc2650ef6b8d90377eb20c7
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-05-25 22:38:40 +0300

    commit

--- Fundamentals/1. More Exercise - Basic Syntax, Conditional Statements and Loops/02. English Name of the Last Digit.js
+++ Fundamentals/1. More Exercise - Basic Syntax, Conditional Statements and Loops/02. English Name of the Last Digit.js
@@ -7,12 +7,34 @@ function login(input) {
     
     for (let i = username.length-1; i >= 0; i--) {
         password = password + username[i];
+        break;
     }
 
-    console.log (password);
+    if (password === "0") {
+        console.log("zero")
+    } else if (password === "1") {
+        console.log("one")
+    } else if (password === "2") {
+        console.log("two")
+    } else if (password === "3") {
+        console.log("three")
+    } else if (password === "4") {
+        console.log("four")
+    } else if (password === "5") {
+        console.log("five")
+    } else if (password === "6") {
+        console.log("six")
+    } else if (password === "7") {
+        console.log("seven")
+    } else if (password === "8") {
+        console.log("eight")
+    } else if (password === "9") {
+        console.log("nine")
+    }
+
     
     } 
-    login(["Hello"]);
-    login(["SoftUni"]);
-    login(["12345"]);
+    login(["512"]);
+    login(["1"]);
+    login(["1643"]);
 
--- Fundamentals/1. More Exercise - Basic Syntax, Conditional Statements and Loops/04. Reverse String.js
+++ Fundamentals/1. More Exercise - Basic Syntax, Conditional Statements and Loops/04. Reverse String.js
@@ -1,19 +1,15 @@
 function reverseString(input) {
               
-    let index = 0;
-    let username = input[index];
-    index++;
     let string = '';
     
-    for (let i = username.length-1; i >= 0; i--) {
-        string = string + username[i];
+    for (let i = input.length - 1; i >= 0; i--) {
+        string = string + input[i];
     }
 
     console.log (string);
     
     } 
-    reverseString(["Hello"]);
-    reverseString(["SoftUni"]);
-    reverseString(["12345"]);
+    reverseString(`Hello`);
+    reverseString(`SoftUni`);
+    reverseString(`12345`);
 
-    ........................ как да махна тази нула
