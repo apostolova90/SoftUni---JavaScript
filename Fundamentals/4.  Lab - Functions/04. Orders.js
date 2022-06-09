952fd021e91a596cf4c27c96924f41781dce1a20
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-06-09 14:12:26 +0300

    commit

--- Fundamentals/4.  Lab - Functions/04. Orders.js
+++ Fundamentals/4.  Lab - Functions/04. Orders.js
@@ -1,7 +1,60 @@
-function orders(){
+function orders(product, quantity){
 
+    let total = 0;
 
+    switch(product){
+        case 'coffee': 
+            total = quantity * 1.50
+            break;
+        case 'coke': 
+            total = quantity * 1.40
+            break;
+        case 'water': 
+            total = quantity * 1
+            break;
+        case 'snacks': 
+            total = quantity * 2
+            break;
+    }
+
+    console.log(total.toFixed(2));
 }
-orders()
-orders()
-orders()
+orders("water", 5)
+orders("coffee", 2)
+
+
+
+// с arrow fumction
+
+/* function orders(product, quantity){
+
+    let total = 0;
+    let coffeeSum = () => quantity * 1.50
+    let cokeSum = () => quantity * 1.40
+    let waterSum = () => quantity * 1
+    let snacksSum = () => quantity * 2
+
+    switch(product){
+        case 'coffee':             
+            total = coffeeSum()
+            break;
+
+        case 'coke':             
+            total = cokeSum()
+            break;
+
+        case 'water':             
+            total = waterSum()
+            break;
+
+        case 'snacks':             
+            total = snacksSum()
+            break;
+    }
+
+    console.log(total.toFixed(2));
+}
+
+orders("water", 5)
+orders("coffee", 2)
+ */
--- Fundamentals/4.  Lab - Functions/06. Sign Check.js
+++ Fundamentals/4.  Lab - Functions/06. Sign Check.js
@@ -1,7 +1,66 @@
-function signCheck(){
+function signCheck(num1, num2, num3) {
 
+    let totalNegative = 0;
+
+    if (isNegative(num1)) {
+        totalNegative++
+    }
+    if (isNegative(num2)) {
+        totalNegative++
+    }
+    if (isNegative(num3)) {
+        totalNegative++
+    }
+
+
+    if (totalNegative % 2 === 0){  // ако от три числа, две са отрицателни, отговора винаги ще е Positive :)
+        console.log ("Positive")
+    } else {
+        console.log ("Negative")
+    }
+
+
+
+    function isNegative(number) { //boolean
+        return number < 0
+    }
 
 }
-signCheck()
-signCheck()
-signCheck()
+
+
+console.log (signCheck(5, 12, -15))
+console.log (signCheck(-6, -12, 14))
+console.log (signCheck(-1, -2, -3))
+console.log (signCheck(-5, 1, 1))
+
+
+
+
+// обикновено решение
+
+/* function signCheck(num1, num2, num3) {
+
+    let totalNegative = 0;
+    for(let i = 0; i < 3; i++){
+        if (num1 < 0) {
+            totalNegative++
+        }
+        if (num2 < 0) {
+            totalNegative++
+        }
+        if (num3 < 0) {
+            totalNegative++
+        }
+    }
+
+    if (totalNegative % 2 === 0){  // ако от три числа, две са отрицателни, отговора винаги ще е Positive :)
+        console.log ("Positive")
+    } else {
+        console.log ("Negative")
+    }
+}
+
+signCheck(5, 12, -15)
+signCheck(-6, -12, 14)
+signCheck(-1, -2, -3)
+signCheck(-5, 1, 1) */
