c1dd18f2bff3742a626848d0f173fa5f43378902
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-06-11 18:42:51 +0300

    commit

--- Fundamentals/Exams/02. Programming Fundamentals Mid Exam/02. Array Modifier.js
+++ Fundamentals/Exams/02. Programming Fundamentals Mid Exam/02. Array Modifier.js
@@ -31,7 +31,29 @@
     }
     console.log(workArr.join(', '))
  
-} */
+} 
+arrayMod([
+  '23 -2 321 87 42 90 -123',
+  'swap 1 3',
+  'swap 3 6',
+  'swap 1 0',
+  'multiply 1 2',
+  'multiply 2 1',
+  'decrease',
+  'end'
+])
+
+arrayMod([
+  '1 2 3 4',
+  'swap 0 1',
+  'swap 1 2',
+  'swap 2 3',
+  'multiply 1 2',
+  'decrease',
+  'end'
+]
+)
+*/
 
 
 ................................................................
