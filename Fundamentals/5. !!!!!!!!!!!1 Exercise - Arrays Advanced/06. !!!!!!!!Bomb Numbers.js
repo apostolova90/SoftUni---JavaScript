b60e772ccdc05d6c3488d697442539127f6531d3
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-06-17 22:20:03 +0300

    commit

---
+++ Fundamentals/5. !!!!!!!!!!!1 Exercise - Arrays Advanced/06. !!!!!!!!Bomb Numbers.js
@@ -0,0 +1,28 @@
+
+
+//ДРУГ
+
+
+function bombNumbers(arr1, arr2) {
+        let bomb = Number(arr2[0]);
+        let range = Number(arr2[1]);
+        while (arr1.includes(bomb)) {
+            let index = arr1.indexOf(bomb);
+            let elementsToRemove = range * 2 + 1;
+            let startIndex = index - range;
+    
+            if (startIndex < 0) {
+                elementsToRemove += startIndex;
+                startIndex = 0;
+            }
+            arr1.splice(startIndex, elementsToRemove);
+            
+        }
+        console.log(arr1.reduce((a, partial) => a + partial, 0));
+
+    }
+bombNumbers([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);
+bombNumbers([1, 4, 4, 2, 8, 9, 1], [9, 3]);
+bombNumbers([1, 7, 7, 1, 2, 3], [7, 1]);
+bombNumbers([1, 1, 2, 1, 1, 1, 2, 1, 1, 1], [2, 1]);
+
