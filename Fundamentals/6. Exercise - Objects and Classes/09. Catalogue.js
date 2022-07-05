88ecf31c326281ce9beb98499e43ab9a44e4281f
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-07-05 20:24:58 +0300

    commit

--- Fundamentals/6. Exercise - Objects and Classes/09. Catalogue.js
+++ Fundamentals/6. Exercise - Objects and Classes/09. Catalogue.js
@@ -34,3 +34,46 @@ function catalogue(input) {
     'Anti-Bug Spray : 15 ',
     'T-Shirt : 10'
   ])
+
+
+
+
+
+
+
+
+// Another 
+
+ /*  function catalogue(input) {
+    let arr = input.slice(0);
+    arr.sort((a, b) => a.localeCompare(b));
+    let abc = {};
+    let char = '';
+    let arrey = [];
+    for (let el of arr) {
+        if (el.charAt(0) !== char) {
+            arrey = [];
+            char = el.charAt(0);
+            abc[char] = arrey;
+            arrey.push(el.replace(' : ', ': '))
+        } else {
+            arrey.push(el.replace(' : ', ': '))
+        }
+    }
+    for (let key of Object.keys(abc)) {
+        console.log(key);
+        for (let el of abc[key]) {
+            console.log(`  ${el}`);
+        }
+    }
+}
+  catalogue([
+    'Apricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15 ',
+    'T-Shirt : 10']) 
+    */
--- Fundamentals/6. More Exercises - Objects and Classes/02. Flight Schedule.js
+++ Fundamentals/6. More Exercises - Objects and Classes/02. Flight Schedule.js
@@ -1,44 +1,43 @@
-
-//ДРУГ
-
+// решение с масивите
 
 function flightSchedule(input) {
-    const flights = input.shift(); // Array for all the flights on a specific sector
-    const newStatuses = input.shift(); // Array with the newly changed statuses
-    const statusToCheck = input.shift().toString(); // Single string
+    const flights = input.shift(); // Array for all the flights on a specific sector - ['WN269 Delaware', 'FL2269 Oregon'...........].
+    const newStatuses = input.shift(); // Array with the newly changed statuses - ['DL2120 Cancelled', 'WN612 Cancelled'............]
+    const statusToCheck = input.shift().toString(); // Single string - ['Cancelled']
    
     const allFlights = []; // This array will hold object elements
    
     // Pushing all the initial flights to the array
     for (const elem of flights) {
-      let [plane, destination] = elem.split(' ');
-      let flight = {
+      let [plane, destination] = elem.split(' '); // сплитваме всеки отделен полет - 'WN269 Delaware'
+      let flight = { // създаваме нов обект, в който записваме всичко
         plane,
         destination,
       };
-      allFlights.push(flight);
+      allFlights.push(flight); // пушваме обекта в масива
     }
    
     // Changing the statuses of the existing flights
-    for (const elem of newStatuses) {
-      let [plane, status] = elem.split(' ');
+    for (const elem of newStatuses) { //минаваме през елемтите във втория масив 
+      let [plane, status] = elem.split(' '); // и пак ги сплитваме
       allFlights.forEach(flight => {
-        if (flight.plane === plane) {
-          flight.status = status;
+        if (flight.plane === plane) { // проверяваме дали полетът е един и същ 
+          flight.status = status; // и ако сменяме статуса му
         }
       });
     }
    
     // Prints all the flights, that its statuses were not changed
-    if (statusToCheck === 'Ready to fly') {
-      allFlights.forEach(flight => {
-        if (!flight.status) {
-          flight.status = 'Ready to fly';
-          console.log(`{ Destination: '${flight.destination}', Status: '${flight.status}' }`);
+    if (statusToCheck === 'Ready to fly') { //ако последният стринг е 'Ready to fly'
+      allFlights.forEach(flight => { // минаваме през всички полети
+        if (!flight.status) { // ако обекта няма статус, му добавя такъв с текст "Ready to fly"
+          flight.status = 'Ready to fly'; 
+          console.log(`{ Destination: '${flight.destination}', Status: '${flight.status}' }`);// принтираме полети, чийто статус не е бил променян
         }
       });
     } else {
-      // Prints all the flights, that its statuses were changed
+      // Prints all the flights, that it`s statuses were changed
+      // ако полета има статус, принтираме всички полети със статус
       allFlights.forEach(flight => {
         if (flight.status) {
           console.log(`{ Destination: '${flight.destination}', Status: '${flight.status}' }`);
