346005377d407920fced3bfd8b7d70750d2cd8d9
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-07-15 12:45:01 +0300

    commit

--- Fundamentals/Exams/Exam Final prep/01. Programming Fundamentals Final Exam Retake/01. The Imitation Game.js
+++ Fundamentals/Exams/Exam Final prep/01. Programming Fundamentals Final Exam Retake/01. The Imitation Game.js
@@ -1,10 +1,65 @@
+function theImitationGame(array){
+
+  let string = array.shift();   //взимаме първата част върху която ще работим
+
+  for (let i = 0; i < array.length; i++){ //въртим през всеки елемент от масива
+    let tokens = array[i].split("|") // и разделяме отделните части на елемента
+    let command = tokens[0]; // и записваме всяки един от тях под отделна променлива
+    
+    if (command === "Decode"){
+      break;
+    }
+
+    if (command === "ChangeAll"){   // заменям (change) всички инстанции от дадено нещо, с друго
+      let substring = tokens[1];
+      let replacement = tokens[2];
+      while (string.includes(substring)){ // задължително с WHILE, за да мине през всички букви, ав слуяай че някоя се повтаря повече от 1 път
+        string = string.replace(substring, replacement)
+      }
+    }
+
+    if (command === "Insert"){ // добавяме даденп нещо след определено нещо в думата
+      let index = Number(tokens[1]);
+      let value = tokens[2];
+      let firstPart = string.substring(0, index); // substring разделя стринга на две, като в случая започва от нулева позиция и продължава до дадения индекс
+      let secondPart = string.substring(index, string.length); // substring разделя стринга на две, като в случая започва от дадения индекс и продължава до края на стринга
+      string = firstPart + value + secondPart
+  
+    }
+
+    if (command === "Move"){ // местим определена част от думата на друго място в думата
+      let index2 = Number(tokens[1]);
+      let firstPart = string.substring(0, index2)
+      let secondPart = string.substring(index2, string.length)
+      string = secondPart + firstPart
+    }
+  }
+
+  console.log(`The decrypted message is: ${string}`)
+
+}
+theImitationGame(["zzHe", "ChangeAll|z|l", "Insert|2|o", "Move|3", "Decode"]);
+/* theImitationGame(["owyouh","Move|2","Move|3","Insert|3|are","Insert|9|?","Decode"]); */
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 //АЗ + споделена с колегите
 
 
-function theImitationGame(array) {
+/* function theImitationGame(array) {
   let message = array.shift(); //взимаме първата част върху която ще работим
 
   for (let line of array) {
@@ -29,10 +84,10 @@ function theImitationGame(array) {
         let firstPart = message.substring(0, index); // substring разделя стринга на две, като в случая започва от нулева позиция и продължава до дадения индекс
         let secondPart = message.substring(index, message.length); // substring разделя стринга на две, като в случая започва от дадения индекс и продължава до края на стринга
         message = firstPart + second + secondPart;
-        /*             let index = Number(first);
-            let messageArray = message.split('')
-            messageArray.splice(index, 0, second);
-            message = messageArray.join() */
+          //            let index = Number(first);
+          //  let messageArray = message.split('')
+          //  messageArray.splice(index, 0, second);
+          //  message = messageArray.join()
         break;
       case "Move":
         let index2 = Number(first);
@@ -44,7 +99,7 @@ function theImitationGame(array) {
   }
 
   console.log(`The decrypted message is: ${message}`);
-}
 
+}
 theImitationGame(["zzHe", "ChangeAll|z|l", "Insert|2|o", "Move|3", "Decode"]);
-theImitationGame(["owyouh","Move|2","Move|3","Insert|3|are","Insert|9|?","Decode",]);
+theImitationGame(["owyouh","Move|2","Move|3","Insert|3|are","Insert|9|?","Decode",]); */
