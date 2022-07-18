3a027e43c2386ce81d99e6f50d9e8d1321a7bded
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-07-18 16:43:33 +0300

    commit

--- Fundamentals/Exams/Exam Final prep/03. Programming Fundamentals Final Exam Retake/01. Secret Chat.js
+++ Fundamentals/Exams/Exam Final prep/03. Programming Fundamentals Final Exam Retake/01. Secret Chat.js
@@ -1,108 +1,61 @@
+// АЗБУЧЕН
 
-// друг
+function secretChat(array){
 
-
-/* function secretChat(arr) {
-
-    let message = arr.shift();
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === "Reveal") {
-            break;
+    let string = array.shift();   //взимаме първата част върху която ще работим
+  
+    for (let i = 0; i < array.length; i++){ //въртим през всеки елемент от масива
+      let tokens = array[i].split(":|:") // и разделяме отделните части на елемента
+      let command = tokens[0]; // и записваме всяки един от тях под отделна променлива
+      
+      if (command === "Reveal"){
+        break;
+      }
+  
+      if (command === "ChangeAll"){   // заменям (change) всички инстанции от дадено нещо, с друго
+        let substring = tokens[1];
+        let replacement = tokens[2];
+        while(string.includes(substring)){
+            string = string.replace(substring, replacement);
         }
-        let tokens = arr[i].split(':|:');
-        let command = tokens[0];
-        if (command === "InsertSpace") {
-            let index = Number(tokens[1]);
-            let firstPart = message.substring(0, index);
-            let secondPart = message.substring(index, message.length);
-            message = firstPart + ' ' + secondPart;
-            console.log(message);
-        } else if (command === "Reverse") {
-            let substring = tokens[1];
-            if (message.includes(substring)) {
-                let firstIndex = message.indexOf(substring);
-                let lastIndex = firstIndex + substring.length;
-                let firstPart = message.substring(0, firstIndex);
-                let secondPart = message.substring(lastIndex, message.length);
-                let reversed = '';
-                for (let i = substring.length - 1; i >= 0; i--) {
-                    reversed += substring[i];
-                }
-                message = firstPart + secondPart + reversed;
-                console.log(message);
+        console.log(string);
+      }
+  
+      if (command === "InsertSpace"){ // добавяме дадено нещо на определено място в думата
+        let index = Number(tokens[1]);
+        let firstPart = string.substring(0, index);
+        let secondPart = string.substring(index, string.length)
+        string = firstPart + " " + secondPart
+        console.log(string);
+      }
+  
+      if (command === "Reverse"){ // отделяме, завъртаме (REVERSE) и добавяме отново, определена част от текста 
+        let substring = tokens[1]
+            if (string.includes(substring)) { // ако определан част присъства в текста
+                let firstIndex = string.indexOf(substring); // намираме старт индекса - от който започва в текста
+                let lastIndex = firstIndex + substring.length; // намираме end индекса - на който завършва
+                let firstPart = string.substring(0, firstIndex); // оделяме първата част 
+                let secondPart = string.substring(lastIndex, string.length); // отделяме втората част 
+                let reversed = substring.split('').reverse().join(''); // сплитваме, завъртаме и пак събраме елемнтите от останалия текст в substring
+                string = firstPart + secondPart + reversed;
+                console.log(string);
             } else {
                 console.log("error");
             }
-        } else if (command === "ChangeAll") {
-            let substring = tokens[1];
-            let replacement = tokens[2];
-            while (message.includes(substring)) {
-                message = message.replace(substring, replacement);
-            }
-            console.log(message);
         }
+
     }
-    console.log(`You have a new text message: ${message}`);
-}
-
-secretChat([
-    'Hiware?uiy',
-    'ChangeAll:|:i:|:o',
-    'Reverse:|:?uoy',
-    'Reverse:|:jd',
-    'InsertSpace:|:3',
-    'InsertSpace:|:7',
-    'Reveal'
-]) */
-
-
-// Anothere
-
-/* function secretChat(input) {
-    let hiddenMessage = input.shift();
-    while (input[0] !== "Reveal") {
-        let commandLine = input.shift().split(":|:")
-        let name = commandLine[0];
-        let p1 = commandLine[1];
-        let p2 = commandLine[2];
-        if (name === 'InsertSpace') {
-            let index = p1;
-            let firstPart = hiddenMessage.slice(0, index);
-            let secondPart = hiddenMessage.slice(index)
-            hiddenMessage = firstPart + " " + secondPart;
-            console.log(hiddenMessage);
-        } else if (name === "Reverse") {
-            let str = p1;
-            if (hiddenMessage.includes(str)) {
-                hiddenMessage = hiddenMessage.replace(str, "");
-                let newStr = str.split("").reverse().join("");
-                hiddenMessage = hiddenMessage + newStr;
-                console.log(hiddenMessage);
-            } else {
-                console.log("error");
-            }
-            continue;
-
-        } else if (name === "ChangeAll") {
-            let substring = p1;
-            let replacement = p2;
-            while (hiddenMessage.includes(substring)) {
-                hiddenMessage = hiddenMessage.replace(substring, replacement);
-            }
-            console.log(hiddenMessage);
-        }
-    }
-    console.log(`You have a new text message: ${hiddenMessage}`)
-}
-secretChat([
+  
+    console.log(`You have a new text message: ${string}`);
+  
+  }
+  secretChat([
     'heVVodar!gniV',
     'ChangeAll:|:V:|:l',
     'Reverse:|:!gnil',
     'InsertSpace:|:5',
-    'Reveal'
-]);
-console.log('---');
+    'Reveal'])
+    
 secretChat([
     'Hiware?uiy',
     'ChangeAll:|:i:|:o',
@@ -110,5 +63,4 @@ secretChat([
     'Reverse:|:jd',
     'InsertSpace:|:3',
     'InsertSpace:|:7',
-    'Reveal'
-]); */
+    'Reveal'])
