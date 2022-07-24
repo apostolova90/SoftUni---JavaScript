54d096ba8f37c0d89b64274bdc8c5ad1f50e02ea
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-07-24 09:22:54 +0300

    commit

--- Fundamentals/09. Regular Expressions/Exercise/03. SoftUni Bar Income.js
+++ Fundamentals/09. Regular Expressions/Exercise/03. SoftUni Bar Income.js
@@ -1,9 +1,48 @@
+function barIncome(input) {
+
+    
+    let totalSum = 0;
+    let pattern = /%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*[|](?<count>\d+)[|][^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$/g;
+    /* /%(?<name>[A-Z]{1}[a-z]+)%[^$%|.]*\<(?<product>\w+)\>[^$%|.]*\|(?<count>\d+)[^$%|.]*\|(?<price>\d+\.\d+)?\$/gm; */
+    
+    let text = input.join('-')
+    let current = pattern.exec(text); //виж бележките в while-a
+
+    while (current){ //същото като while((current = pattern.exec(text))), но в точи случай трябва да презапишем стойността (...)
+
+        let currentPrice = current.groups.count * current.groups.price;
+        
+        console.log(`${current.groups.name}: ${current.groups.product} - ${currentPrice.toFixed(2)}`);
+
+        totalSum += currentPrice
+
+        current = pattern.exec(text); //(...) по този начин
+    }
+
+    console.log(`Total income: ${totalSum.toFixed(2)}`)
+    
+}
+barIncome(['%George%<Croissant>|2|10.3$',
+'%Peter%<Gum>|1|1.3$',
+'%Maria%<Cola>|1|2.4$',
+'end of shift']);
+
+console.log('-------------');
+
+barIncome(['%InvalidName%<Croissant>|2|10.3$',
+'%Peter%<Gum>1.3$',
+'%Maria%<Cola>|1|2.4',
+'%Valid%<Valid>valid|10|valid20$',
+'end of shift']);
+
+
 
 // Another 
 
-function softuniBarIncome(arr) {
+/* function softuniBarIncome(arr) {
 
     let regex = /%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$/;
+    
 
     let total = 0;
 
@@ -36,4 +75,4 @@ softuniBarIncome([
 '%Peter%<Gum>1.3$',
 '%Maria%<Cola>|1|2.4',
 '%Valid%<Valid>valid|10|valid20$',
-'end of shift'])
+'end of shift']) */
---
+++ Fundamentals/09. Regular Expressions/Exercise/04. Star Enigma (не влиза в крайната оценка).js
@@ -0,0 +1,48 @@
+function starEnigma(input){
+
+    let messages = Number(input.shift()) // звимам броя на съобщенията
+
+    let regex = /@([A-Z][a-z]+)[^@\-!:>]*:\d+[^@\-!:>]*!(A|D)![^@\-!:>]*->\d+/;
+    let attacked = [];
+    let destroyed = [];
+    // search for: [s, t, a, r] 
+
+    for (let i = 0; i < messages; i++){ // минавам през всеки отделен стринг в инпута
+        let encryptedMessages = input[i]   
+        
+        let count = encryptedMessages.toLowerCase().split("").filter((char) => char === "s" || char === "t" || char === "a" || char === "r").length // правим всички букви, във всеки стринг, малки  ['stcdoghudd4=63333$d$0a53333']   
+        // сплитваме, за да получа всяка буква ['s', 't', 'c', 'd', 'o', 'g', 'h', 'u', 'd', 'd', '4', '=', '6', '3', '3', '3', '3', '$', 'd', '$', '0', 'a', '5', '3', '3', '3', '3']
+        // филтрираме, за да извадим сало буквите които търсим
+        // и накрая с .length взимаме дължината на стринга, който сме получили
+
+    let decryptedMessage = encryptedMessages.split("").map((c) => String.fromCharCode(c.charCodeAt(0) - count)).join("")
+        // за всеки знак от стринга, взимаме числовата му равностойност от ASCII таблицата, 
+        // вадим от нея count-a 
+        // и след това обръщаме числото, в новия знак от ASCII таблицата
+
+    let regexMatch = decryptedMessage.match(regex);
+    if (regexMatch) { // проверяваме дали има regex match
+        if (regexMatch[2] === "A"){ // и коя от двете букви има - "A" за attacked ИЛИ "D" за destroyed
+            attacked.push(regexMatch[1]) // ако има, пушваме името на планетата в масива attacked
+        } else if (regexMatch[2] === "D"){
+            destroyed.push(regexMatch[1]) // ако има, пушваме името на планетата в масива attacked
+        }
+    }
+}
+
+console.log(`Attacked planets: ${attacked.length}`);
+Object.entries(attacked).sort((a,b) => a[1].localeCompare(b[1])).forEach((p) => console.log(`-> ${p[1]}`))
+console.log(`Destroyed planets: ${destroyed.length}`);
+Object.entries(destroyed).sort((a,b) => a[1].localeCompare(b[1])).forEach((p) => console.log(`-> ${p[1]}`))
+
+}
+starEnigma([
+'2',
+'STCDoghudd4=63333$D$0A53333',
+'EHfsytsnhf?8555&I&2C9555SR'])
+
+starEnigma([
+'3',
+"tt(''DGsvywgerx>6444444444%H%1B9444",
+'GQhrr|A977777(H(TTTT',
+'EHfsytsnhf?8555&I&2C9555SR'])
