5d7b1135dfda1c3a42bdf5ffd3e8cc7ad5971b25
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-07-26 17:21:37 +0300

    commit

--- Fundamentals/09. Regular Expressions/More Exercises/01. Winning Ticket.js
+++ Fundamentals/09. Regular Expressions/More Exercises/01. Winning Ticket.js
@@ -1,74 +1,11 @@
-// AZ 
-//       30/100
-
-function winningTicket(input){
-
-  let regex = /[#|$|^|@]{6,10}/g;
-  let tickets = input.trim().split(/\s*,\s*/)
-
-  let message = ""
-
-  for (let el of tickets){
-
-          let elementLength = el.length
-
-        if (elementLength < 20){
-            console.log(`invalid ticket`)
-          } else {
-      
-          let firstPart = el.substring(0, (el.length / 2))
-          let secondPart = el.substring((el.length / 2), el.length)
-
-          let symbolsFirstPart = firstPart.match(regex);
-          let symbolsSecondPart = secondPart.match(regex);
-
-          if(symbolsFirstPart === null && symbolsSecondPart === null){
-            console.log(`ticket "${el}" - no match`)
-            break;
-          }
-
-
-          let arrSymbolsFirstPart = symbolsFirstPart.shift().split("")
-          let char = arrSymbolsFirstPart[0]
-          let length = arrSymbolsFirstPart.length
-          let repeats = length * 2
-          symbolsFirstPart = arrSymbolsFirstPart.join("").toString()
-          symbolsSecondPart = arrSymbolsFirstPart.join("").toString()
-          
-
-    if (symbolsFirstPart === symbolsSecondPart){
-    
-      if (repeats >= 12 && repeats < 18) {
-        message = `ticket "${el}" - ${length}${char}`;
-    } else if (repeats >= 20){
-        message = `ticket "${el}" - ${length}${char} Jackpot!`;
-    } 
-    console.log(message);
-      }
-    }   
-  } 
-
-}
-/* winningTicket(`Cash$$$$$$Ca$$$$$$sh`);
-console.log("____________")
-winningTicket(`$$$$$$$$$$$$$$$$$$$$, aabb  , th@@@@@@eemo@@@@@@ey`);
-console.log("____________")
-winningTicket(`validticketnomatch:(`);
-console.log("____________") */
-winningTicket(`сс@@@@@@@@@@@@dddddf`)
-
-
-
 
 //Another
 
-100/100
-
-//function winningTicket(str) {
-//    const VALID_TICKET_LENGTH = 20;
-//    const tickets = str.trim().split(/\s*,\s*/);
-//    const pattern = /(?=.{20}).*?(?=(?<char>[@#$^]))(?<match>\k<char>{6,}).*(?<=.{10})\k<match>.*/;
- /*  
+function winningTicket(str) {
+const VALID_TICKET_LENGTH = 20;
+const tickets = str.trim().split(/\s*,\s*/);
+const pattern = /(?=.{20}).*?(?=(?<char>[@#$^]))(?<match>\k<char>{6,}).*(?<=.{10})\k<match>.*/;
+  
     for (const ticket of tickets) {
       const validTicket = pattern.exec(ticket);
   
@@ -103,4 +40,4 @@ winningTicket(`сс@@@@@@@@@@@@dddddf`)
   
   console.log('----------------------Test 3----------------------');
   winningTicket('validticketnomatch:(');
-   */
+  
--- Fundamentals/Exams/Exam Final prep/04. Programming Fundamentals Final Exam/02. Fancy Barcodes.js
+++ Fundamentals/Exams/Exam Final prep/04. Programming Fundamentals Final Exam/02. Fancy Barcodes.js
@@ -1,40 +1,56 @@
 
+//АЗ
 
-// друг
+function fancyBarcodes(input) {
 
+let number = Number(input.shift());
+    
 
-/* function fancyBarcodes(arr) {
+for (let i = 0; i < number; i++){  // завъртаме през дадения предварително брой редове
+    let regex = /@#{1,}(?<pass>[A-Z][A-Za-z0-9]{4,}[A-Z])@#{1,}/g
+    let numbers = []
+    let match = regex.exec(input[i]) // проверяваме дали отделните стрингове, отговарят на заданените в regex-a параметри
+// match = (2) ['@###Val1d1teM@###', 'Val1d1teM', index: 0, input: '@###Val1d1teM@###', groups: {…}]
+    if (!match) { 
+        console.log(`Invalid barcode`)
+    }
 
-    let count = Number(arr.shift());
+    while (match !== null){ // ако проверката връща различно от null
+        let food = match.groups['pass']; // създаваме нова променлива, в която записваме всеки отговарящ елемнт
 
-    let regex = /(\@)(\#{1,})[A-Z]([a-zA-Z0-9]{4,})[A-Z]\1(\#{1,})/;
-
-    for (let i = 0; i < count; i++) {
-        let group = '';
-        if ((valid = regex.exec(arr[i])) !== null) {
-            for (let char of valid[0]) {
-                if (char >= 0 && char <= 9) {
-                    group += char;
-                }
+        for (let char of food) {
+            if ((char.charCodeAt(0) >= 48) && (char.charCodeAt(0) <= 57)) { // по този начин проверяваме дали char-a е число или буква
+                numbers.push(char); // ако е число, го push-ваме в масива
             }
-            if (group.length > 0) {
-                console.log(`Product group: ${group}`);
-            } else {
-                console.log(`Product group: 00`);
-            }
-        } else {
-            console.log("Invalid barcode");
         }
+        if (numbers.length < 1){
+            console.log("Product group: 00")
+        } else {
+            console.log(`Product group: ${numbers.join("")}`)
+        }
+        
+    match = regex.exec(input[i])
     }
 }
 
-fancyBarcodes(["6",
-    "@###Val1d1teM@###",
-    "@#ValidIteM@#",
-    "##InvaliDiteM##",
-    "@InvalidIteM@",
-    "@#Invalid_IteM@#",
-    "@#ValiditeM@#"]) */
+}
+fancyBarcodes([
+"3",
+"@#FreshFisH@#",
+"@###Brea0D@###",
+"@##Che4s6E@##"]);
+
+console.log('---');
+
+fancyBarcodes([
+"6",
+"@###Val1d1teM@###",
+"@#ValidIteM@#",
+"##InvaliDiteM##",
+"@InvalidIteM@",
+"@#Invalid_IteM@#",
+"@#ValiditeM@#"]);
+
 
 
 
@@ -81,3 +97,46 @@ fancyBarcodes([
     "@#Invalid_IteM@#",
     "@#ValiditeM@#"
 ]); */
+
+
+
+
+
+
+// Another
+
+
+/* function fancyBarcodes(arr) {
+
+    let count = Number(arr.shift());
+    let regex = /(\@)(\#{1,})[A-Z]([a-zA-Z0-9]{4,})[A-Z]\1(\#{1,})/;
+
+    for (let i = 0; i < count; i++) {
+        let group = '';
+        if ((valid = regex.exec(arr[i])) !== null) {
+            for (let char of valid[0]) {
+                if (char >= 0 && char <= 9) {
+                    group += char;
+                }
+            }
+            if (group.length > 0) {
+                console.log(`Product group: ${group}`);
+            } else {
+                console.log(`Product group: 00`);
+            }
+        } else {
+            console.log("Invalid barcode");
+        }
+    }
+}
+
+fancyBarcodes(["6",
+    "@###Val1d1teM@###",
+    "@#ValidIteM@#",
+    "##InvaliDiteM##",
+    "@InvalidIteM@",
+    "@#Invalid_IteM@#",
+    "@#ValiditeM@#"]) */
+
+
+
