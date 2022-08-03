fbe46ac27bee69102ed0fabdacc374398844337f
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-08-03 12:39:46 +0300

    commit

--- Fundamentals/Exams/Exam Final prep/03. Programming Fundamentals Final Exam Retake/02. Mirror Words.js
+++ Fundamentals/Exams/Exam Final prep/03. Programming Fundamentals Final Exam Retake/02. Mirror Words.js
@@ -1,8 +1,55 @@
+// АЗ
+
+function mirrorWords(text) {
+
+    let regex = /(@|#)(?<firstWord>[A-Za-z]{3,})\1\1(?<secondWord>[A-Za-z]{3,})\1/g;
+    let allMirrorWords = [];
+    let counter = 0;
+
+    let match = regex.exec(text) // откриваме всички Mirror Words в текста ---  (7) ['#poOl##loOp#', '#', 'poOl', '#', '#', 'loOp', '#', index: 11, input: '@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$lo…@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r', groups: {…}]
+
+    if (!match) {  // ако regex-a НЕ открива нищо
+        console.log("No word pairs found!");
+        console.log("No mirror words!");
+    } else { // ако открива:
+        while (match !== null){ // въртим докато вече няма matches
+            counter++ // бройм всяка двойка думи, без значение дали е mirror или не
+ 
+            let firstWord = match.groups['firstWord'] // взимаме първата дума
+            let secondWord = match.groups['secondWord'] // взимаме втората
+            let reversed = secondWord.split("").reverse().join("") // обръщаме втората
+                if (firstWord === reversed){ // проверяваме дали са еднакви и ако СА:
+                    allMirrorWords.push(`${firstWord} <=> ${secondWord}`) // ги наливаме в масив ---> (3) ['Part <=> traP', 'leveL <=> Level', 'sAw <=> wAs']
+                }
+            match = regex.exec(text) // ЗАДЪЛЖИТЕЛНО занулявам данните, за да мине на следваща позиция
+        }
+        let mirrorWordsToPrint = allMirrorWords.join(", ") // превръшам масива в стринг ['Part <=> traP', 'leveL <=> Level', 'sAw <=> wAs']   ===>   Part <=> traP, leveL <=> Level, sAw <=> wAs
+        console.log(`${counter} word pairs found!`) // принтирам броя на откритите двойки думи
+        if (allMirrorWords.length === 0){ // проверявам дали има открити някакви mirror words (ако няма, масива ще е празен, т.е. = 0)
+            console.log("No mirror words!") // ако няма, принтирам това
+        } else { // ако има принитрам това:
+            console.log("The mirror words are:")  
+            console.log(mirrorWordsToPrint)     
+        }
+    }
+
+}
+mirrorWords(['@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r'])
+console.log("_________")
+mirrorWords([`#po0l##l0op# @bAc##cAB@ @LM@ML@ #xxxXxx##xxxXxx# @aba@@ababa@`])
+console.log("_________")
+mirrorWords([`#lol#lol# @#God@@doG@# #abC@@Cba# @Xyu@#uyX#`])
+
+
+
+
+
+
 
 
 // АЗ
 
-function mirrorWords(data) {
+/* function mirrorWords(data) {
 
     let regex = /([@|#])(?<firstword>[A-Za-z]{3,})\1{2}(?<secondWord>[A-Za-z]{3,})\1/g //{2} е задължително след \1, защото думите започват и завършват с един от двата знака (@, #) и ако не сложим {2}  няма да хванем случайте, когато знака присъства 2 пъти един след друг
     let firstWord = ""
@@ -43,6 +90,7 @@ function mirrorWords(data) {
 mirrorWords(['@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r'])
 mirrorWords([`#po0l##l0op# @bAc##cAB@ @LM@ML@ #xxxXxx##xxxXxx# @aba@@ababa@`])
 mirrorWords([`#lol#lol# @#God@@doG@# #abC@@Cba# @Xyu@#uyX#`])
+ */
 
 
 
@@ -51,8 +99,7 @@ mirrorWords([`#lol#lol# @#God@@doG@# #abC@@Cba# @Xyu@#uyX#`])
 
 
 
-
-// друг
+// Another
 
 
 /* function mirrorWords(arr) {
