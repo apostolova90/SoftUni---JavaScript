cd5d31a2d39f00a411808b618215fe5c0039a54a
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-08-07 23:12:37 +0300

    commit

---
+++ Basics/5. While Loop/More Exercises/03.Stream Of Letters.js
@@ -0,0 +1,57 @@
+function streamOfLetters(input) {
+    let index = 0;
+    let command = input[index];
+    let secretCommand = 0;
+    let word = "";
+    let finalWord = "";
+    let lastCommand = "";
+    while (command !== "End") {
+      let letter = command;
+   
+      if (
+        (command >= "A" && command <= "Z") ||
+        (command >= "a" && command <= "z")
+      ) {
+   
+        if (command === "c" || command === "o" || command === "n") {
+   
+          if (lastCommand === command && secretCommand !== 0) {
+            word += letter + "";
+            index++;
+            command = input[index];
+            continue;
+          }
+   
+          secretCommand++;
+   
+          if (
+            (command === "c" || command === "o" || command === "n") &&
+            secretCommand <= 3
+          ) {
+            lastCommand = command;
+          }
+   
+          index++;
+          command = input[index];
+   
+          if (secretCommand === 3) {
+            secretCommand = 0;
+            finalWord = word;
+            word = word + " ";
+          }
+          continue;
+        }
+   
+        word += letter + "";
+        index++;
+        command = input[index];
+   
+      } else {
+        index++;
+        command = input[index];
+      }
+    }
+    
+    console.log(finalWord);
+  }
+  streamOfLetters(["H","n","e","l","l","o","o","c","t","c","h","o","e","r","e","n","e","End"])
