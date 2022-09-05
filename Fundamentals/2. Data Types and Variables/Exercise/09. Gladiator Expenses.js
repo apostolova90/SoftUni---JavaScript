dcee746256404f5dee233ee1e85c648f10185de9
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-09-05 12:57:33 +0300

    commit

--- Fundamentals/2. Data Types and Variables/Exercise/09. Gladiator Expenses.js
+++ Fundamentals/2. Data Types and Variables/Exercise/09. Gladiator Expenses.js
@@ -1,1 +1,30 @@
-ц
+function gladiatorExpenses(lostFights, hPrice, swPrice, shPrice, aPrice) {
+
+    let sum = 0;
+    let shBreakCount = 0;
+
+    for (let i = 1; i <= lostFights; i++) {
+        if (i % 2 == 0 && i % 3 == 0) {
+            sum += hPrice;
+            sum += swPrice;
+            sum += shPrice;
+            shBreakCount++;
+            if (shBreakCount % 2 == 0) {
+                sum += aPrice;
+            }
+        } else {
+            if (i % 2 == 0) {
+                sum += hPrice;
+            } else if (i % 3 == 0) {
+                sum += swPrice;
+            }
+        }
+    }
+    console.log(`Gladiator expenses: ${sum.toFixed(2)} aureus`);
+}
+
+gladiatorExpenses(23,
+    12.50,
+    21.50,
+    40,
+    200)
