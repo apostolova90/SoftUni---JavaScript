43fa4890cfbbda5a371bdfd72f1388c935d18f89
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-09-13 12:37:29 +0300

    commit

--- Basics/1. First Steps In Coding/Exercise/9 - Fish Tank.js
+++ Basics/1. First Steps In Coding/Exercise/9 - Fish Tank.js
@@ -1,6 +1,6 @@
 function fishTank(input){
-    const lenght = Number(input[0]);
-    const widht = Number(input[1]);
+    const length = Number(input[0]);
+    const width = Number(input[1]);
     const height = Number(input[2]);
     const otherThingsInTheTank = Number(input[3]);
     const totalCapacity = lenght * widht * height
