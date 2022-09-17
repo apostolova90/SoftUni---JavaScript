457d228d304e293e45ad3dd98a3a71190cee9e85
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-09-17 11:08:46 +0300

    commit

--- Basics/1. First Steps In Coding/More Exercises/09. Weather Forecast.js
+++ Basics/1. First Steps In Coding/More Exercises/09. Weather Forecast.js
@@ -1,10 +1,9 @@
 function weatherForecast (input){
 
     let weather = input[0];
-if (weather === "sunny"){
+    if (weather === "sunny"){
     console.log ("It's warm outside!")
-
-    }else {
+    } else {
     console.log ("It's cold outside!")
     }
 }
