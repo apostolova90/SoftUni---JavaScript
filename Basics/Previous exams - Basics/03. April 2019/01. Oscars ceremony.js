4135aa5d160c7f5799317eb62a950a9a47b1e500
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-10-03 09:05:57 +0300

    commit

--- Basics/Previous exams - Basics/03. April 2019/01. Oscars ceremony.js
+++ Basics/Previous exams - Basics/03. April 2019/01. Oscars ceremony.js
@@ -2,10 +2,10 @@ function oscarCeremony(input) {
     let priceRoom = Number(input[0]);
 
     let priceStatue = priceRoom - priceRoom * 0.30;
-    let priceCetaring = priceStatue - priceStatue * 0.15;
-    let priceMusic = priceCetaring / 2;
+    let priceCatering = priceStatue - priceStatue * 0.15;
+    let priceMusic = priceCatering / 2;
 
-    let totalPrice = priceRoom + priceStatue + priceCetaring + priceMusic;
+    let totalPrice = priceRoom + priceStatue + priceCatering + priceMusic;
 
     console.log (totalPrice.toFixed(2));
 }
