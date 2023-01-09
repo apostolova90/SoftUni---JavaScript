84fe976e46d663bd511801457d6bbea941df5c0f
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-01-09 07:42:33 +0200

    Update 06. sumOfNumbersN...M.js

--- Advanced/1. Syntax, Functions and Statements/Lab/06. sumOfNumbersN...M.js
+++ Advanced/1. Syntax, Functions and Statements/Lab/06. sumOfNumbersN...M.js
@@ -1,10 +1,14 @@
-function sumOfNumbers(numOne, numTwo){
+function sumOfNumbers(num1, num2){
 
-    let sum;
+    let sum = 0;
+    let numOne = Number(num1);
+    let numTwo = Number(num2);
 
     for (let i = numOne; i <= numTwo; i++){
-        sum += i
+        sum += i;
     }
 
     console.log(sum)
 }
+sumOfNumbers('1', '5');
+sumOfNumbers('-8', '20')
