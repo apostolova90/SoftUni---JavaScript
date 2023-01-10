27456922b61af5568443f03ea4d03215272c365e
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-01-10 09:11:29 +0200

    Update 02. greatestCommonDivisor.js

--- Advanced/1. Syntax, Functions and Statements/Exercise/02. greatestCommonDivisor.js
+++ Advanced/1. Syntax, Functions and Statements/Exercise/02. greatestCommonDivisor.js
@@ -1,3 +1,23 @@
+function greatestCommonDivisor(a, b) {
+    let firstNumber = Number(a);
+    let secondNumber = Number(b);
+
+    while(firstNumber !== secondNumber){ // въртим докато първото и второто число са различни
+
+        if (firstNumber > secondNumber){ // в зависимост от това, кое число е по-голямо, от него вадим по-малкото
+            firstNumber -= secondNumber; 
+        } else {
+            secondNumber -= firstNumber
+        }
+    }
+    console.log(firstNumber)
+}
+greatestCommonDivisor(15, 5);
+greatestCommonDivisor(2154, 458)
+
+
+
+/* 
 function greatestCommonDivisor(firstNum, secondNum) {
     const maxNum = Math.max(firstNum, secondNum); // кое е най-голямото число от дадените
         let gcd;
@@ -10,3 +30,8 @@ function greatestCommonDivisor(firstNum, secondNum) {
 }
 greatestCommonDivisor(15, 5);
 greatestCommonDivisor(2154, 458)
+ */
+
+
+
+
