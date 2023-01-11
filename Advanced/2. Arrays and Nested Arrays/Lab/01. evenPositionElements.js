3c20d8ec0a2635d7caa33537b303bc6eb0d27bf7
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-01-11 10:09:10 +0200

    Update 01. evenPositionElements.js

---
+++ Advanced/2. Arrays and Nested Arrays/Lab/01. evenPositionElements.js
@@ -0,0 +1,30 @@
+function evenPositionElement(array){
+
+    let allEvenPosition = []; // масив, в който да наливаме
+    for (let i = 0; i < array.length; i++) { // минаваме през всяка позиция в масива
+        if (i % 2 === 0) {// проверяваме дали позицията е четна или нечетна
+            allEvenPosition.push(array[i]); // ако е четна я пушваме в новия масив
+        }
+    }
+
+    console.log(allEvenPosition.join(` `))
+}
+evenPositionElement(['20', '30', '40', '50', '60']);
+evenPositionElement(['5', '10'])
+
+
+
+// Another
+/* function evenPositionElement(array){
+
+    let allEvenPosition = ''; 
+    for (let i = 0; i < array.length; i++) { 
+        if (i % 2 === 0) {
+            allEvenPosition += array[i] + ' ';
+        }
+    }
+
+    console.log(allEvenPosition)
+}
+evenPositionElement(['20', '30', '40', '50', '60']);
+evenPositionElement(['5', '10']) */
