7ab31d92e41784a6bb2b02a555d020e112d90e7b
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-01-12 09:00:18 +0200

    Update 06. biggerHalf.js

---
+++ Advanced/2. Arrays and Nested Arrays/Lab/06. biggerHalf.js
@@ -0,0 +1,14 @@
+function biggerHalf(array){
+
+    let newArray = [];
+    let length = Math.ceil(array.length / 2);
+    array.sort((a,b) => a - b).reverse();
+
+    for (let i = 0; i < length; i++){
+        newArray.push(array[i]);
+    }
+
+    return newArray.reverse();
+}
+/* biggerHalf(biggerHalf([4, 7, 2, 5])); */
+biggerHalf([3, 19, 14, 7, 2, 19, 6])
