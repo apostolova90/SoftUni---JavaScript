d3fc3a5151afbb6987662022ed98bdcdfb04729c
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-01-19 10:47:58 +0200

    Create 01. calorieObject.js

---
+++ Advanced/3. Objects and Composition/Exercise/01. calorieObject.js
@@ -0,0 +1,15 @@
+function caloriesObject(array){
+
+    let obj = {};
+    
+    for (let i = 0; i < array.length; i+=2){
+        let productName = array[i];
+        let calories = Number(array[i+1]);
+
+        // слагаме всичко в обекта
+        obj[productName] = calories;
+    }
+    console.log(obj)
+}
+caloriesObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
+/* caloriesObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']) */
