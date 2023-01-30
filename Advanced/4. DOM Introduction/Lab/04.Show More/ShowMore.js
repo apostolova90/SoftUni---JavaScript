297967805339c41c44e53ac4d7d1ebc8e9a49d6a
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-01-30 10:14:55 +0200

    Create ShowMore.js

---
+++ Advanced/4. DOM Introduction/Lab/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+
+    let hiddenTextElement = document.getElementById('text');
+    hiddenTextElement.style.display = 'inline'; // показваме допълнителния текст
+
+    let shoMoreElement = document.getElementById('more');
+    shoMoreElement.style.display = 'none'; // скриваме "show more" 
+}
