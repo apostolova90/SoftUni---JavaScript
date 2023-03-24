70f1641b3ce5e851580246d8f71140327015a5f7
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-03-24 20:45:23 +0200

    Update 2. Serialize String.js

--- Fundamentals/8. Text Processing/More Exercise/2. Serialize String.js
+++ Fundamentals/8. Text Processing/More Exercise/2. Serialize String.js
@@ -26,36 +26,3 @@ function serializeString(arr) {
 serializeString(["avjavamsdmcalsdm"])
 
 
-/* function serializeString(arr) {
-
-    let string = arr[0];
-    let stringArr = [];
-    let uniqueLettersArr = [];
-
-    for (let i = 0; i < string.length; i++) {
-        stringArr.push(string[i]);
-    }
-
-    uniqueLettersArr.push(stringArr.shift());
-
-    for (let letter of stringArr) {
-        if (!uniqueLettersArr.includes(letter)) {
-            uniqueLettersArr.push(letter);
-        }
-    }
-
-    let finalArr = [];
-
-    for (let letter of uniqueLettersArr) {
-        for (let i = 0; i < string.length; i++) {
-            if (string[i] === letter) {
-                finalArr.push(i);
-            }
-        }
-        console.log(`${letter}:${finalArr.join('/')}`);
-        finalArr = [];
-    }
-
-}
-
-serializeString(["avjavamsdmcalsdm"]) */
