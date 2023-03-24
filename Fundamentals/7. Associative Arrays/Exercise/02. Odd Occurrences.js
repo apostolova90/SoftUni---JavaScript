f339377a096770c8a43348eca0b53d05c7eb13dd
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-03-24 20:45:27 +0200

    Update 02. Odd Occurrences.js

--- Fundamentals/7. Associative Arrays/Exercise/02. Odd Occurrences.js
+++ Fundamentals/7. Associative Arrays/Exercise/02. Odd Occurrences.js
@@ -1,6 +1,5 @@
 
 // АЗ
-
 // по колко пъти се повтарят всички думи в масив
 
 function oddOccurrences(input){
@@ -30,42 +29,5 @@ function oddOccurrences(input){
     console.log(resultArray.join(' '));
 
 }
-oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
-oddOccurrences('Cake IS SWEET is Soft CAKE sweet Food')
-
-
-
-
-
-
-// с Точко, с Map
-
-/* function oddOccurrences(input){
-
-    let result =new Map();
-    let words = input.split(" "); // разделяме всички думи в масива - ['Java', 'C#', 'Php', 'PHP', 'Java', 'PhP', '3', 'C#', '3', '1', '5', 'C#']
-
-    for (let word of words){
-        word = word.toLowerCase()
-        if (result.has(word)){
-            let oldValue = result.get(word) //взиамаме старата стойност
-            result.set(word, oldValue + 1) // и добавяме към старата стойност 1
-        } else {
-            result.set(word, 1) //ако думата не се повтаря, а се среща само 1 път, създаваме нова стойност към key
-        }
-    }
-
-    let filterResult = Array.from(result).filter(([key, value]) => { // .from връща масив от масиви, .filter изкарва ключ и стойност
-        return value % 2 !== 0    //връща само стойностите, които отговарят на условието
-    }) 
-
-    let buff = "";
-    for (let [key, value] of filterResult){
-        buff += key + " "
-    }
-
-    console.log(buff)
-
-}
-oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
-oddOccurrences('Cake IS SWEET is Soft CAKE sweet Food') */
+oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#');
+oddOccurrences('Cake IS SWEET is Soft CAKE sweet Food');
