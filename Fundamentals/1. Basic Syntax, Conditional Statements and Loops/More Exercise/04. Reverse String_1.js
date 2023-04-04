8610800c561fae467ba60be4df36f6cdc7e7ced3
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-04-04 11:15:14 +0300

    Create 04. Reverse String_1.js

---
+++ Fundamentals/1. Basic Syntax, Conditional Statements and Loops/More Exercise/04. Reverse String_1.js
@@ -0,0 +1,14 @@
+function reverseString(input) {
+              
+    let string = '';
+    
+    for (let i = input.length - 1; i >= 0; i--) {
+        string = string + input[i];
+    }
+
+    console.log (string);
+    
+    } 
+    reverseString(`Hello`);
+    reverseString(`SoftUni`);
+    reverseString(`12345`);
