0435233f2565b138e9ad2f2f6c4dfc639120d26f
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-04-04 11:15:10 +0300

    Create 04. Reverse String_2.js

---
+++ Fundamentals/1. Basic Syntax, Conditional Statements and Loops/More Exercise/04. Reverse String_2.js
@@ -0,0 +1,12 @@
+
+
+// друго решение
+
+
+function reverseString(str){
+    const newStr = str.split('').reverse().join('');
+   console.log(newStr);
+ }
+ reverseString(`Hello`);
+ reverseString(`SoftUni`);
+ reverseString(`12345`);
