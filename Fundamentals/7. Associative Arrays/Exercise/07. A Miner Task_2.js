911a86fa24f7a1f489fd6a80ccc71b15ac0e75bd
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-04-08 15:25:05 +0300

    Create 07. A Miner Task_2.js

---
+++ Fundamentals/7. Associative Arrays/Exercise/07. A Miner Task_2.js
@@ -0,0 +1,38 @@
+//ДРУГ
+// с обект
+
+function aMinerTask(array) {
+ 
+    let resources = {};
+ 
+    for (let i = 0; i < array.length; i+=2){
+        let resource = array[i];
+ 
+        if (!resources.hasOwnProperty(resource)) {
+            resources[resource] = 0
+        }
+        resources[resource] +=Number(array[i+1])
+    }
+ 
+    for (let resource in resources) {
+        console.log(`${resource} -> ${resources[resource]}`)
+    }
+}
+ 
+aMinerTask([
+    'Gold',
+    '155',
+    'Silver',
+    '10',
+    'Copper',
+    '17']);
+
+aMinerTask([
+    'gold',
+    '155',
+    'silver',
+    '10',
+    'copper',
+    '17',
+    'gold',
+    '15'])
