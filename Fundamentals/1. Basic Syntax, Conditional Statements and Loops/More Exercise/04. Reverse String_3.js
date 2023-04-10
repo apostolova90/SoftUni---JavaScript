757d6f9b7b25b145ff0c0f9faf4c135314fd4839
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-04-10 12:54:47 +0300

    Create 04. Reverse String_3.js

---
+++ Fundamentals/1. Basic Syntax, Conditional Statements and Loops/More Exercise/04. Reverse String_3.js
@@ -0,0 +1,11 @@
+
+// Another
+
+function reverseString(string){
+    let stringSplitted = string.split('');
+    let stringReversed = stringSplitted.reverse();
+    let output = stringReversed.join('')
+
+    console.log(output);
+}
+reverseString('Andrea')
