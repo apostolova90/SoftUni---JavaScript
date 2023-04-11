5683901fefb34fdf8411f2415ac19456f6a37b1b
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-04-11 12:35:06 +0300

    Create 05. Distance between Points_2.js

---
+++ Fundamentals/1. Basic Syntax, Conditional Statements and Loops/More Exercise/05. Distance between Points_2.js
@@ -0,0 +1,9 @@
+
+// Another
+
+function distanceBetweenPoints(x1, y1, x2, y2) {
+    let result = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+   
+    console.log(result);
+}
+distanceBetweenPoints(2.34, 15.66, -13.55, -2.9985)
