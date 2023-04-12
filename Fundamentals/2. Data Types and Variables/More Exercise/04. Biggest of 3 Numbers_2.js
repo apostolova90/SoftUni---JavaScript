a50b1377e2d6a933af76bd8799d9f1ee8e0b35bd
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-04-12 10:10:25 +0300

    Create 04. Biggest of 3 Numbers_2.js

---
+++ Fundamentals/2. Data Types and Variables/More Exercise/04. Biggest of 3 Numbers_2.js
@@ -0,0 +1,16 @@
+
+// Another
+
+function biggest(a,b,c){
+    if (a >= b && a >= c){
+        console.log(a);
+    } else if (b >= a && b >= c){
+        console.log(b);
+    } else {
+        console.log(c);
+    }
+}
+biggest(-2,7,3)
+biggest(130,5,99)
+biggest(43,43.2,43.1)
+biggest(2,2,2)
